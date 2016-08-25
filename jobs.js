6753bc1a0f14241152eaa338ed1d3fc181dbdb5e
taijia <taijia@tangtaijia.com>
2016-08-26 01:52:55 +0800

    fetch jobs retry if error

--- jobs.js
+++ jobs.js
@@ -27,16 +27,20 @@ var self = module.exports = function (key, pageNo, callback) {
         process.setMaxListeners(0);
         request.post(options, function (error, response, html) {
             if (!error) {
-                var result = JSON.parse(html);
-                pageCount = parseInt(result.content.data.page.totalCount / pageSize) + (result.content.data.page.totalCount % pageSize ? 1 : 0);
-                util.log('fetch joblist json with proxy: ' + proxy_url + ', page:' + options.form.pageNo + '/' + pageCount + ', key:' + key);
-                jobs = jobs.concat(result.content.data.page.result);
-                if (options.form.pageNo < pageCount) {
-                    sleep(5000);
-                    self(key, ++options.form.pageNo, callback);
+                if(html.indexOf('DOCTYPE') > -1) {
+                    self(key, options.form.pageNo, callback);
+                } else {
+                    var result = JSON.parse(html);
+                    pageCount = parseInt(result.content.data.page.totalCount / pageSize) + (result.content.data.page.totalCount % pageSize ? 1 : 0);
+                    util.log('fetch joblist json with proxy: ' + proxy_url + ', page:' + options.form.pageNo + '/' + pageCount + ', key:' + key);
+                    jobs = jobs.concat(result.content.data.page.result);
+                    if (options.form.pageNo < pageCount) {
+                        sleep(5000);
+                        self(key, ++options.form.pageNo, callback);
+                    }
+                    else
+                        callback(jobs);
                 }
-                else
-                    callback(jobs);
             } else {
                 console.error(error);
                 if (callback)
