3c9fc3a69871fc6cac2490416c1a09610b5d3826
taijia <taijia@tangtaijia.com>
2016-09-04 22:20:12 +0800

    add some domains

--- proxyfetcher.js
+++ proxyfetcher.js
@@ -13,10 +13,15 @@ var detail = yargs.d || false;
 var local = yargs.l || false;
 var try_count = 0;
 var urlsArr = [
-    // ['http://cn-proxy.com/', 'http://cn-proxy.com/archives/218'],
-    // ['http://proxy.com.ru/', 'http://proxy.com.ru/touming/', 'http://proxy.com.ru/niming/', 'http://proxy.com.ru/gaoni/'],
+    ['http://cn-proxy.com/', 'http://cn-proxy.com/archives/218'],
+    ['http://proxy.com.ru/', 'http://proxy.com.ru/touming/', 'http://proxy.com.ru/niming/', 'http://proxy.com.ru/gaoni/'],
     ['http://www.xicidaili.com/nn/', 'http://www.xicidaili.com/nt/', 'http://www.xicidaili.com/wn/', 'http://www.xicidaili.com/wt/'],
-    ['http://www.kuaidaili.com/free/']
+    ['http://www.kuaidaili.com/free/'],
+    ['http://proxylist.hidemyass.com/'],
+    ['https://incloak.com/proxy-list/?country=CNJPKRTWUS&type=h'],
+    ['https://www.us-proxy.org/', 'http://www.sslproxies.org/', 'http://free-proxy-list.net/uk-proxy.html', 'http://free-proxy-list.net/anonymous-proxy.html'],
+    ['http://gatherproxy.com/proxylist/country/?c=China', 'http://gatherproxy.com/proxylist/country/?c=United%20States', 'http://gatherproxy.com/proxylist/country/?c=Taiwan'],
+    ['http://proxydb.net/']
 ];
 
 var runTask = exports.runTask = function (pcallback) {
@@ -26,14 +31,14 @@ var runTask = exports.runTask = function (pcallback) {
         callbacks = [];
         urls.forEach(function (url, uindex) {
             callbacks.push(function (callback) {
-                sleep(2000);
+                sleep(100);
                 fetchIps(url, callback);
             });
         });
         callbacksArr.push(callbacks);
     });
 
-    async.mapLimit(callbacksArr, 2, function (callbacks, callback) {
+    async.mapLimit(callbacksArr, 5, function (callbacks, callback) {
         async.series(callbacks,
             function (err, results) {
                 callback(null, results);
@@ -48,11 +53,15 @@ var saveIps = function (result, url, callback) {
     if (detail)
         util.log(JSON.stringify(result, null, 2));
     util.log(url, 'fetch', result.length);
-    taskworker.addIps(result, function (err, reply) {
-        util.log(url, 'save', reply);
-        if (callback)
-            callback(null, result);
-    });
+    if (result && result.length) {
+        taskworker.addIps(result, function (err, reply) {
+            util.log(url, 'save', reply);
+            if (callback)
+                callback(null, result);
+        });
+    } else if (callback)
+        callback(null, result);
+
 };
 
 var fetchIps = function (url, callback) {
@@ -97,13 +106,25 @@ var fetchIps = function (url, callback) {
                         if (next_page.length && next_page.text().replace(/[^\d]/g, '') < 6) {
                             nexturl = current_uri.protocol + '//' + current_uri.host + next_page.find('a').attr('href');
                         }
+                    } else if (url.indexOf('http://proxylist.hidemyass.com/') > -1) {
+                        current_page = $('ul.pagination li.current');
+                        next_page = current_page.next();
+                        if (next_page.length && next_page.text().replace(/[^\d]/g, '') < 6) {
+                            nexturl = current_uri.protocol + '//' + current_uri.host + next_page.find('a').attr('href');
+                        }
+                    } else if (url.indexOf('http://proxydb.net/') > -1) {
+                        current_page = $('ul.pagination li.active');
+                        next_page = current_page.next();
+                        if (next_page.length && next_page.text().replace(/[^\d]/g, '') < 6) {
+                            nexturl = current_uri.protocol + '//' + current_uri.host + next_page.find('a').attr('href');
+                        }
                     }
 
                     var nextcall = callback;
                     if (nexturl) {
                         nextcall = function () {
                             fetchIps(nexturl, callback);
-                            sleep(2000);
+                            sleep(4000);
                         };
                     }
                     saveIps(proxyips_arr, url, nextcall);
@@ -164,6 +185,70 @@ var parseIps = function (url, $, callback) {
         });
     } else if (url.indexOf('http://www.kuaidaili.com/') > -1) {
         $('#list table').children('tbody').children('tr').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('td').eq(0).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(0).text().trim(),
+                    'port': $(element).find('td').eq(1).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (url.indexOf('http://proxylist.hidemyass.com/') > -1) {
+        $('#listable table').children('tbody').children('tr').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('td').eq(1).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(1).text().trim(),
+                    'port': $(element).find('td').eq(2).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (url.indexOf('https://incloak.com/proxy-list/') > -1) {
+        $('.proxy__t').children('tbody').children('tr').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('td').eq(0).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(0).text().trim(),
+                    'port': $(element).find('td').eq(1).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (
+        url.indexOf('www.us-proxy.org') > -1 ||
+        url.indexOf('www.sslproxies.org') > -1 ||
+        url.indexOf('free-proxy-list.net') > -1
+    ) {
+        $('#proxylisttable').children('tbody').children('tr').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('td').eq(0).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(0).text().trim(),
+                    'port': $(element).find('td').eq(1).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (url.indexOf('http://gatherproxy.com/proxylist/country/') > -1) {
+        $('.proxy-list .table').children('ul').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('li').eq(1).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(1).text().trim(),
+                    'port': $(element).find('td').eq(2).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (url.indexOf('https://hidester.com/proxylist/') > -1) {
+        $('.proxyListTable table').children('tr').each(function (index, element) {
+            if (/[\d{1,3}\.]/.test($(element).find('li').eq(1).text().trim())) {
+                var new_ip = {
+                    'ip': $(element).find('td').eq(1).text().trim(),
+                    'port': $(element).find('td').eq(2).text().trim()
+                };
+                proxyips_arr.push(new_ip);
+            }
+        });
+    } else if (url.indexOf('http://proxydb.net/') > -1) {
+        $('.table-responsive table').children('tbody').children('tr').each(function (index, element) {
             if (/[\d{1,3}\.]/.test($(element).find('td').eq(0).text().trim())) {
                 var new_ip = {
                     'ip': $(element).find('td').eq(0).text().trim(),
